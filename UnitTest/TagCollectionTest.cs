bfed4c80306301e635d5a80c76d0b225356363ba
Tomona Nanase <nanase@users.noreply.github.com>
2015-11-21 22:59:32 +0900

    update: テストクラスTagCollectionTestを更新

--- UnitTest/TagCollectionTest.cs
+++ UnitTest/TagCollectionTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sharp98;
 
@@ -7,5 +8,93 @@ namespace UnitTest
     [TestClass]
     public class TagCollectionTest
     {
+        [TestMethod]
+        public void ConstructorTest1()
+        {
+            var tag = new TagCollection();
+            Assert.AreEqual(0, tag.Count);
+        }
+
+        [TestMethod]
+        public void ConstructorTest2()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add("title", "foo");
+            tag_base.Add("name", "bar");
+
+            var tag_buffer = tag_base.Export();
+            var new_tag = new TagCollection(tag_buffer);
+            Assert.AreEqual(2, new_tag.Count);
+            Assert.AreEqual("foo", new_tag["title"]);
+            Assert.AreEqual("bar", new_tag["name"]);
+
+            var sjis = Encoding.GetEncoding("Shift_JIS");
+            var tag_buffer_sjis = tag_base.Export(sjis);
+            var new_tag_sjis = new TagCollection(tag_buffer, sjis);
+            Assert.AreEqual(2, new_tag.Count);
+            Assert.AreEqual("foo", new_tag["title"]);
+            Assert.AreEqual("bar", new_tag["name"]);
+        }
+
+        [TestMethod]
+        public void ConstructorTest3()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add("title", "foo");
+            tag_base.Add("name", "bar");
+            
+            var new_tag = new TagCollection(tag_base);
+            Assert.AreEqual(2, new_tag.Count);
+            Assert.AreEqual("foo", new_tag["title"]);
+            Assert.AreEqual("bar", new_tag["name"]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddError1()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add(null, "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddError2()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add("title", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddError3()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add(string.Empty, "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddError4()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add("title\n", "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddError5()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add("title=", "foo");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddError6()
+        {
+            var tag_base = new TagCollection();
+            tag_base.Add("title", "foo\n");
+        }
     }
 }

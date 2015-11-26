00d06a32c392c46b682d1dc50f4a95a4188038c9
Tomona Nanase <nanase@users.noreply.github.com>
2015-11-26 21:12:07 +0900

    add: DeviceInfoにXML Documentを追加

--- Sharp98/DeviceType.cs
+++ Sharp98/DeviceType.cs
@@ -28,52 +28,122 @@
 
 namespace Sharp98
 {
+    /// <summary>
+    /// 対象デバイスを識別するための列挙体です。
+    /// </summary>
+    /// <remarks>
+    /// <see cref="DeviceType"/> 列挙体は使用されるデバイスを指定する際に、デバイスのタイプを識別するために使われます。
+    /// 原典: http://www.vesta.dti.ne.jp/~tsato/soft_s98v3.html
+    /// </remarks>
     public enum DeviceType : uint
     {
+        /// <summary>
+        /// 未使用。対象データは無視されます。
+        /// </summary>
         None = 0,
 
-        //
+        // --- ---
 
+        /// <summary>
+        /// YM2149 (PSG)。
+        /// </summary>
         YM2149 = 1,
 
+        /// <summary>
+        /// YM2203 (OPN)。
+        /// </summary>
         YM2203 = 2,
 
+        /// <summary>
+        /// YM2612 (OPN2)。
+        /// </summary>
         YM2612 = 3,
 
+        /// <summary>
+        /// YM2608 (OPNA)。
+        /// </summary>
         YM2608 = 4,
 
+        /// <summary>
+        /// YM2151 (OPM)。
+        /// </summary>
         YM2151 = 5,
 
+        /// <summary>
+        /// YM2413 (OPLL)。
+        /// </summary>
         YM2413 = 6,
 
+        /// <summary>
+        /// YM3526 (OPL)。
+        /// </summary>
         YM3526 = 7,
 
+        /// <summary>
+        /// YM3812 (OPL2)。
+        /// </summary>
         YM3812 = 8,
 
+        /// <summary>
+        /// YMF262 (OPL3)。
+        /// </summary>
         YMF262 = 9,
 
+        /// <summary>
+        /// AY-3-8910 (PSG)。
+        /// </summary>
         AY_3_8910 = 15,
 
+        /// <summary>
+        /// SN76489 (DCSG)。
+        /// </summary>
         SN76489 = 16,
 
-        //
+        // --- ---
         
+        /// <summary>
+        /// OPN (YM2203)。
+        /// </summary>
         OPN = YM2203,
 
+        /// <summary>
+        /// OPN2 (YM2612)。
+        /// </summary>
         OPN2 = YM2612,
 
+        /// <summary>
+        /// OPNA (YM2608)。
+        /// </summary>
         OPNA = YM2608,
 
+        /// <summary>
+        /// OPM (YM2151)。
+        /// </summary>
         OPM = YM2151,
 
+        /// <summary>
+        /// OPLL (YM2413)。
+        /// </summary>
         OPLL = YM2413,
 
+        /// <summary>
+        /// OPL (YM3526)。
+        /// </summary>
         OPL = YM3526,
 
+        /// <summary>
+        /// OPL2 (YM3812)。
+        /// </summary>
         OPL2 = YM3812,
 
+        /// <summary>
+        /// OPL3 (YMF262)。
+        /// </summary>
         OPL3 = YMF262,
 
+        /// <summary>
+        /// DCSG (SN76489)。
+        /// </summary>
         DCSG = SN76489,
     }
 }

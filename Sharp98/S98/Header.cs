6ed9e51553015d89ec2968f6f5fafdb03570a2d3
Tomona Nanase <nanase@users.noreply.github.com>
2015-11-28 13:45:28 +0900

    change: Header#Importを最適化

--- Sharp98/S98/Header.cs
+++ Sharp98/S98/Header.cs
@@ -165,32 +165,21 @@ namespace Sharp98.S98
                 throw new ArgumentNullException(nameof(stream));
 
             if (!stream.CanRead)
-                throw new InvalidOperationException();
+                throw new InvalidOperationException("読み取りができないストリームが指定されました.");
 
             if (!stream.CanSeek)
-                throw new InvalidOperationException();
+                throw new InvalidOperationException("シークができないストリームが指定されました.");
 
             var buffer = new byte[32];
             byte[] tagBuffer;
-            int buffer_int, loop_index = 0;
+            int loop_index = 0;
             DeviceInfo[] di;
             TagCollection tagCollection;
-            List<DumpData> dumpData = new List<DumpData>();
+            IReadOnlyList<DumpData> dumpData;
             uint timerInfo, timerInfo2;
             uint tag, dump, loop, device_count;
 
             stream.Read(buffer, 0, 32);
-
-            // Magic / Version
-            {
-                for (int i = 0; i < magicNumber.Length; i++)
-                    if (buffer[i] != magicNumber[i])
-                        throw new InvalidDataException();
-
-                if (buffer[3] != 0x33)
-                    throw new InvalidDataException();
-            }
-
             timerInfo = buffer.GetLEUInt32(4);
             timerInfo2 = buffer.GetLEUInt32(8);
             tag = buffer.GetLEUInt32(16);
@@ -198,70 +187,26 @@ namespace Sharp98.S98
             loop = buffer.GetLEUInt32(24);
             device_count = buffer.GetLEUInt32(28);
 
-            if (tag > stream.Length)
-                throw new InvalidDataException();
+            if (!CheckMagicAndVersion(buffer, 0))
+                throw new InvalidDataException("不明なファイル形式、またはバージョンです.");
+
+            if ((tag < dump) || (tag > stream.Length))
+                throw new InvalidDataException("タグ情報のオフセットが範囲外です.");
 
             if (dump > stream.Length)
-                throw new InvalidDataException();
+                throw new InvalidDataException("ダンプ情報のオフセットが範囲外です.");
 
-            if (loop > stream.Length)
-                throw new InvalidDataException();
-
-            if (loop != 0 && loop < dump)
-                throw new InvalidDataException();
-
-            if (tag < dump)
-                throw new InvalidDataException();
-
+            if ((loop > stream.Length) || (loop != 0 && loop < dump))
+                throw new InvalidDataException("ループ位置のオフセットが範囲外です.");
+            
             if (device_count > 64)
-                throw new InvalidDataException();
-
-            if (device_count == 0)
-                di = new DeviceInfo[] { DeviceInfo.DefaultDevice };
-            else
-            {
-                di = new DeviceInfo[device_count];
-                stream.Read(buffer, 0, 16);
-
-                for (int i = 0; i < device_count; i++)
-                    di[i] = DeviceInfo.Import(buffer);
-            }
-
+                throw new InvalidDataException("デバイス数が 64 個を超えています.");
+            
+            di = ImportDevice(device_count, stream, buffer);
+            
             stream.Seek(dump, SeekOrigin.Begin);
-            while (true)
-            {
-                if (stream.Position == loop)
-                    loop_index = dumpData.Count;
-
-                buffer_int = stream.ReadByte();
-                stream.Seek(-1L, SeekOrigin.Current);
-
-                if (buffer_int >= 0 && buffer_int < 128)
-                    stream.Read(buffer, 0, 3);
-                else if (buffer_int == 0xff)
-                    stream.Read(buffer, 0, 1);
-                else if (buffer_int == 0xfd)
-                {
-                    stream.Read(buffer, 0, 1);
-                    dumpData.Add(S98.DumpData.Import(buffer));
-                    break;
-                }
-                else if (buffer_int == 0xfe)
-                {
-                    stream.Read(buffer, 0, 6);
-                    int arrayLength = Array.FindIndex(buffer, 1, 5, e => e < 128);
-
-                    if (arrayLength == -1)
-                        throw new InvalidDataException();
-                    else
-                        stream.Seek(arrayLength - 5L, SeekOrigin.Current);
-                }
-                else
-                    throw new InvalidDataException();
-
-                dumpData.Add(S98.DumpData.Import(buffer));
-            }
-
+            dumpData = ImportDump(stream, loop, out loop_index, buffer);
+            
             stream.Seek(tag, SeekOrigin.Begin);
             tagBuffer = new byte[stream.Length - tag];
             stream.Read(tagBuffer, 0, tagBuffer.Length);
@@ -297,6 +242,79 @@ namespace Sharp98.S98
             // Device Count
             (this.Device.Count == 0 ? 0 : (uint)this.Device.Count).GetLEByte(buffer, 28);
             stream.Write(buffer, 0, 32);
+        }
+
+        private static bool CheckMagicAndVersion(byte[] buffer, int index = 0)
+        {
+            // magic
+            for (int i = 0; i < magicNumber.Length; i++)
+                if (buffer[i] != magicNumber[i])
+                    return false;
+
+            // version
+            if (buffer[3] != 0x33)
+                return false;
+
+            return true;
+        }
+
+        private static DeviceInfo[] ImportDevice(uint deviceCount, Stream stream, byte[] buffer)
+        {
+            if (deviceCount == 0)
+                return new DeviceInfo[] { DeviceInfo.DefaultDevice };
+            else
+            {
+                var di = new DeviceInfo[deviceCount];
+                stream.Read(buffer, 0, 16);
+
+                for (int i = 0; i < deviceCount; i++)
+                    di[i] = DeviceInfo.Import(buffer);
+
+                return di;
+            }
+        }
+
+        private static IReadOnlyList<DumpData> ImportDump(Stream stream, uint loop_offset, out int loop_index, byte[] buffer)
+        {
+            int buffer_int;
+            var dumpData = new List<DumpData>();
+            loop_index = 0;
+
+            while (true)
+            {
+                if (stream.Position == loop_offset)
+                    loop_index = dumpData.Count;
+
+                buffer_int = stream.ReadByte();
+                stream.Seek(-1L, SeekOrigin.Current);
+
+                if (buffer_int >= 0 && buffer_int < 128)
+                    stream.Read(buffer, 0, 3);
+                else if (buffer_int == 0xff)
+                    stream.Read(buffer, 0, 1);
+                else if (buffer_int == 0xfd)
+                {
+                    stream.Read(buffer, 0, 1);
+                    dumpData.Add(S98.DumpData.Import(buffer));
+                    break;
+                }
+                else if (buffer_int == 0xfe)
+                {
+                    stream.Read(buffer, 0, 6);
+                    int arrayLength = Array.FindIndex(buffer, 1, 5, e => e < 128);
+
+                    if (arrayLength == -1)
+                        throw new InvalidDataException();
+                    else
+                        stream.Seek(arrayLength - 5L, SeekOrigin.Current);
+                }
+                else
+                    throw new InvalidDataException();
+
+                dumpData.Add(S98.DumpData.Import(buffer));
+            }
+
+            return dumpData;
         }
 
         private void ExportDevice(Stream stream)

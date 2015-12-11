aea9e661f81c2f08066d3a229ae22a0fe3bd4557
Tomona Nanase <nanase@users.noreply.github.com>
2015-12-12 05:25:11 +0900

    add: ChannelAndAddress.csを追加

---
+++ Sharp98/DataParameter/ChannelAndAddress.cs
@@ -0,0 +1,58 @@
+﻿//
+// ChannelAndAddress.cs
+//
+// Author:
+//       Tomona Nanase <nanase@users.noreply.github.com>
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Tomona Nanase
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace Sharp98.DataParameter
+{
+    public struct ChannelAndAddress
+    {
+        #region -- Private Fields --
+
+        private readonly int address;
+        private readonly int channel;
+
+        #endregion
+
+        #region -- Public Properties --
+        
+        public int Channel => this.channel;
+
+        public int Address => this.address;
+        
+        #endregion
+
+        #region -- Constructors --
+
+        public ChannelAndAddress(int channel, int address)
+        {
+            this.channel = channel;
+            this.address = address;
+        }
+
+        #endregion
+    }
+}

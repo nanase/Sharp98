fe23ce14445a99746b1ba6d9d1b8465f549359fe
Tomona Nanase <nanase@users.noreply.github.com>
2016-01-01 22:07:26 +0900

    add: DeviceInfoTest.csを追加

---
+++ UnitTest/S98/DeviceInfoTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sharp98.S98;
+
+namespace UnitTest.S98
+{
+    [TestClass]
+    public class DeviceInfoTest
+    {
+        const Sharp98.DeviceType type = Sharp98.DeviceType.OPNA;
+        const Sharp98.S98.DeviceType s98type = Sharp98.S98.DeviceType.OPNA;
+        const int clock = 8000000;
+        const PanFlag pan = PanFlag.Stereo;
+
+        [TestMethod]
+        public void ConstructorTest1()
+        {
+            var device = new DeviceInfo();
+            Assert.AreEqual(Sharp98.DeviceType.None, device.DeviceType);
+            Assert.AreEqual(0, device.Clock);
+            Assert.AreEqual(PanFlag.Stereo, device.Pan);
+            Assert.AreEqual(Sharp98.S98.DeviceType.None, device.S98DeviceType);
+        }
+
+        [TestMethod]
+        public void ConstructorTest2()
+        {
+            var device = new DeviceInfo(s98type, clock, pan);
+            Assert.AreEqual(type, device.DeviceType);
+            Assert.AreEqual(clock, device.Clock);
+            Assert.AreEqual(pan, device.Pan);
+            Assert.AreEqual(s98type, device.S98DeviceType);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorError1()
+        {
+            var device = new DeviceInfo(unchecked((Sharp98.S98.DeviceType)(-1)), clock, pan);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorError2()
+        {
+            var device = new DeviceInfo(s98type, 0, pan);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorError3()
+        {
+            var device = new DeviceInfo(s98type, int.MaxValue + 1u, pan);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ConstructorError4()
+        {
+            var device = new DeviceInfo(s98type, clock, unchecked((PanFlag)(-1)));
+        }
+
+        [TestMethod]
+        public void Export1Test()
+        {
+            var device = new DeviceInfo(s98type, clock, pan);
+
+            var output1 = device.Export(Encoding.UTF8);
+            var output2 = device.Export(null);
+
+            CollectionAssert.AreEqual(output1, output2);
+            Assert.IsNotNull(output1);
+            Assert.AreEqual(16, output1.Length);
+        }
+
+        [TestMethod]
+        public void Export2Test()
+        {
+            var device = new DeviceInfo(s98type, clock, pan);
+            var buffer = new byte[16];
+            var size = device.Export(buffer, 0, 16, null);
+
+            Assert.AreEqual(16, size);
+        }
+
+        [TestMethod]
+        public void Export3Test()
+        {
+            var device = new DeviceInfo(s98type, clock, pan);
+            byte[] buffer;
+
+            using (var ms = new MemoryStream())
+            {
+                device.Export(ms, null);
+                ms.Flush();
+                buffer = ms.ToArray();
+            }
+
+            Assert.AreEqual(16, buffer.Length);
+        }
+    }
+}
